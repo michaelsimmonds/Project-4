3829f42325da89d4f0b835e9245b3f0018c0a5a7
3jtechno <3j.techno@gmail.com>
2019-02-08 15:49:40 +0000

    update mongo db env variable in environment file

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGO_URI || 'mongodb://localhost/Project-4'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/Project-4'
 
 module.exports = {
   port: port,
