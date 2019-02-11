a484665134e45720ee36d524299cae67512e496f
3jtechno <3j.techno@gmail.com>
2019-02-11 16:07:54 +0000

    added user location

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import mapboxgl from 'mapbox-gl'
+const Promise = require('bluebird')
+
+Promise.promisifyAll(navigator.geolocation)
 
 class Dashboard extends React.Component{
 
@@ -7,6 +10,8 @@ class Dashboard extends React.Component{
     super()
 
     this.state = {
+      markersCoord: [],
+      map: {},
       mapDOMElement: '',
       data: {
         username: 'admin',
@@ -31,147 +36,150 @@ class Dashboard extends React.Component{
         ]
       }
     }
-
   }
 
-  componentDidMount(){
+  createMarkups(){
+    console.log(this.markersCoord)
+    this.markersCoord.forEach(geoCoord => {
+
+      const markerDOM = document.createElement('img')
+      markerDOM.setAttribute('class', 'marker')
+      markerDOM.setAttribute('src', 'https://s1.qwant.com/thumbr/0x380/9/3/956b158d13001c1f57346b9fa0932fa2bc0aba7071bdc679d34e390524c1da/2000px-Map_marker.svg.png?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F8%2F88%2FMap_marker.svg%2F2000px-Map_marker.svg.png&q=0&b=1&p=0&a=1')
+
+      const markers = new mapboxgl.Marker({element: markerDOM, anchor: 'bottom'})
+        .setLngLat([String(geoCoord.lng), String(geoCoord.lat)])
+        .addTo(this.map)
+    })
+  }
+
+  createMap(){
 
     mapboxgl.accessToken = process.env.MAPBOX_KEY
-
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: this.mapDOMElement,
       style: 'mapbox://styles/mapbox/streets-v10',
       center: [0, 0],
       zoom: 1
     })
 
-    //Create markers
-    const markers = this.state.data.places.map(place => ({lat: place.geog[0], lng: place.geog[1]}))
-    console.log(markers)
-    // [
-    // {
-    //   lat: 40.730610,
-    //   lng: -73.935242
-    // },
-    // {
-    //   lat: 51.509865,
-    //   lng: -0.118092
-    // },
-    // {
-    //   lat: -14.833071,
-    //   lng: -74.933539
-    // },
-    // {
-    //   lat: 22.286394,
-    //   lng: 114.149139
-    // },
-    // {
-    //   lat: -33.865143,
-    //   lng: 151.209900
-    // }]
+  }
 
-    let angle, opposite, adjacent, direction, move
+  addUserLocalisation(markersCoord){
+    let userLocation = []
+
+    navigator.geolocation
+      .getCurrentPositionAsync()
+      .then(pos => userLocation = pos.coords)
+      .catch(err => console.warn(err))
+
+    if(userLocation) return markersCoord.unshift({lat: userLocation.latitude, lng: userLocation.longitude})
+    else return markersCoord
+
+  }
+
+  getMarkersCoord(){
+    this.markersCoord = this.state.data.places.map(place => {
+      return ({lat: place.geog[0], lng: place.geog[1]})
+    })
+    return true
+  }
+
+  drawTrip(){
+
+    let angle, opposite, adjacent, direction, move, animation
     // number of frames per longitude degree
     const speedFactor = 30
-    let animation
     // to store and cancel the animation
     let startTime = 0
     // progress = timestamp - startTime
     let progress = 0
 
-    map.on('load', function() {
-
-      //Create markup on the map
-      createMarkups()
-
-      // Create a GeoJSON source with an empty lineString.
-      const geojson = {
-        'type': 'FeatureCollection',
-        'features': [{
-          'type': 'Feature',
-          'geometry': {
-            'type': 'LineString',
-            //Starting point
-            'coordinates': [
-              [markers[0].lng, markers[0].lat]
-            ]
-          }
-        }]
-      }
-
-      // add the line which will be modified in the animation
-      map.addLayer({
-        'id': 'line-animation',
-        'type': 'line',
-        'source': {
-          'type': 'geojson',
-          'data': geojson
-        },
-        'layout': {
-          'line-cap': 'round',
-          'line-join': 'round'
-        },
-        'paint': {
-          'line-color': '#ed6498',
-          'line-width': 5,
-          'line-opacity': .8
+    // Create a GeoJSON source with an empty lineString.
+    const geojson = {
+      'type': 'FeatureCollection',
+      'features': [{
+        'type': 'Feature',
+        'geometry': {
+          'type': 'LineString',
+          //Starting point
+          'coordinates': [
+            [this.markersCoord[0].lng, this.markersCoord[0].lat]
+          ]
         }
-      })
+      }]
+    }
 
-      startTime = performance.now()
-
-      //Calculate angle of the trajectory
-      calculateAngleOfTrajectory()
-
-      animateLine()
-
-      function createMarkups(){
-
-        markers.forEach(geoCoord => {
-
-          const markerDOM = document.createElement('img')
-          markerDOM.setAttribute('class', 'marker')
-          markerDOM.setAttribute('src', 'https://s1.qwant.com/thumbr/0x380/9/3/956b158d13001c1f57346b9fa0932fa2bc0aba7071bdc679d34e390524c1da/2000px-Map_marker.svg.png?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F8%2F88%2FMap_marker.svg%2F2000px-Map_marker.svg.png&q=0&b=1&p=0&a=1')
-
-          const newYorkMarker = new mapboxgl.Marker({element: markerDOM, anchor: 'bottom'})
-            .setLngLat([String(geoCoord.lng), String(geoCoord.lat)])
-            .addTo(map)
-        })
+    // add the line which will be modified in the animation
+    this.map.addLayer({
+      'id': 'line-animation',
+      'type': 'line',
+      'source': {
+        'type': 'geojson',
+        'data': geojson
+      },
+      'layout': {
+        'line-cap': 'round',
+        'line-join': 'round'
+      },
+      'paint': {
+        'line-color': '#ed6498',
+        'line-width': 5,
+        'line-opacity': .8
       }
-
-      function calculateAngleOfTrajectory(){
-        opposite = markers[1].lat - markers[0].lat
-        adjacent = markers[1].lng - markers[0].lng
-        angle = Math.atan(opposite / adjacent)
-        direction = markers[1].lng - markers[0].lng > 0 ? 'east' : 'west'
-      }
-
-      function animateLine(timestamp) {
-
-        progress = timestamp - startTime
-
-        if(direction === 'east') move = progress / speedFactor
-        else if(direction === 'west') move = - progress / speedFactor
-
-        var x = move +  markers[0].lng || markers[0].lng // initial point
-        // draw a sine wave with some math.
-        var y = (x - markers[0].lng)  * Math.tan(angle) + markers[0].lat|| markers[0].lat
-        // append new coordinates to the lineString
-        geojson.features[0].geometry.coordinates.push([x, y])
-        // then update the map
-        map.getSource('line-animation').setData(geojson)
-
-        // Request the next frame of the animation.
-        if(x > markers[1].lng && direction === 'east' || x < markers[1].lng && direction === 'west'){
-          markers.shift()
-          if(markers.length < 2) return
-          startTime = timestamp
-          calculateAngleOfTrajectory()
-        }
-        animation = requestAnimationFrame(animateLine)
-      }
-
     })
+
+    startTime = performance.now()
+
+    //Calculate angle of the trajectory
+    calculateAngleOfTrajectory()
+
+    animateLine()
+
+    function calculateAngleOfTrajectory(){
+      opposite = this.markersCoord[1].lat - this.markersCoord[0].lat
+      adjacent = this.markersCoord[1].lng - this.markersCoord[0].lng
+      angle = Math.atan(opposite / adjacent)
+      direction = this.markersCoord[1].lng - this.markersCoord[0].lng > 0 ? 'east' : 'west'
+    }
+
+    function animateLine(timestamp) {
+
+      progress = timestamp - startTime
+
+      if(direction === 'east'){
+        move = progress / speedFactor
+      } else if(direction === 'west') {
+        move = - progress / speedFactor
+      }
+
+      var x = move +  this.markersCoord[0].lng || this.markersCoord[0].lng // initial point
+      // draw a sine wave with some math.
+      var y = (x - this.markersCoord[0].lng)  * Math.tan(angle) + this.markersCoord[0].lat|| this.markersCoord[0].lat
+      // append new coordinates to the lineString
+      geojson.features[0].geometry.coordinates.push([x, y])
+      // then update the map
+      this.map.getSource('line-animation').setData(geojson)
+
+      // Request the next frame of the animation.
+      if(x > this.markersCoord[1].lng && direction === 'east' || x < this.markersCoord[1].lng && direction === 'west'){
+        this.markersCoord.shift()
+        if(this.markersCoord.length < 2) return
+        startTime = timestamp
+        calculateAngleOfTrajectory()
+      }
+      animation = requestAnimationFrame(animateLine)
+    }
+
+  }
+
+  componentDidMount(){
+
+    this.getMarkersCoord()
+      .then(() => this.addUserLocalisation())
+      .then(() => this.createMap())
+      .then(() => this.createMarkups())
+      .then(() => this.map.on('load', () => this.drawTrip()))
+
   }
 
   render(){
