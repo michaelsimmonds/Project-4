e071e1912e328c7a72114c1102c73535585ee0cd
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-02-11 11:22:56 +0000

    Changed the seed file, hardcoded places into user, added places and location array to user

--- models/user.js
+++ models/user.js
@@ -4,7 +4,9 @@ const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  places: { type: Array },
+  location: {type: Array }
 })
 
 userSchema.virtual('passwordConfirmation')
