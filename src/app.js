053e42769da7098ce2a0881bb2e4b1238fe60a1e
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2019-02-11 10:50:27 +0000

    styling added to index page tiles are flipping

--- src/app.js
+++ src/app.js
@@ -8,8 +8,7 @@ import './style.scss'
 
 import NavBar from './components/common/NavBar'
 import Home from './Home'
-import PlacesIndex from './components/places/PlacesIndex'
-import PlacesShow from './components/places/PlacesShow'
+import {PlacesIndex, PlacesShow} from './components/places/'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 
